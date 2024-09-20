005d0fc542da784d2701c336aa0adbc23beece04
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2024-09-20 12:26:29 -0300

    Update idiomas.js

--- algoritimo/idiomas.js
+++ algoritimo/idiomas.js
@@ -1,34 +1,25 @@
-    console.log (" SELECT THE DESIRED LANGUAGE ")
-    console.log (" pt = PORTUGUES ")
-    console.log (" en = INGLES ")
-    console.log (" es = ESPANHOL" )
-    console.log (" fr = FRANÇES")
+
+console.log (" SELECT THE DESIRED LANGUAGE ")
+console.log (" 1 = PORTUGUES ")
+console.log (" 2 = INGLES ")
+console.log (" 3 = ESPANHOL" )
+console.log (" 4 = FRANÇES")
 const prompt = require("prompt-sync")()
 const idiomas= prompt(" escolha um idioma")
 switch (idiomas) {
-  case "pt":
-  console.log ("idioma configurado para portugues")
-  break
-  case "en":
+case "1":
+    console.log ("idioma configurado para portugues")
+break
+case "2":
     console.log ("language set to english")
-    break
-    case "es":
-        console.log ("idioma configurado a espanõl")
-        break
-        case "fr":
-            console.log ("langue definie sur le français")
-            break
-            default:
-            console.log ("idioma não suportado", idiomas)
-
-
-
-
-
-
-
-
-
-
+break
+case "3":
+    console.log ("idioma configurado a espanõl")
+break
+case "4":
+    console.log ("langue definie sur le français")
+break
+    default:
+    console.log ("idioma não suportado", idiomas)
 
 }
