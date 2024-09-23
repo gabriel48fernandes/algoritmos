4693b0f28b57816a11417a93deecf5d84c0bb0fe
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2024-09-23 20:19:15 -0300

    Update saudar.js

--- algoritimo/saudar.js
+++ algoritimo/saudar.js
@@ -2,3 +2,21 @@ const prompt=require("prompt-sync")()
 const nome= prompt(" qual seu nome ? ")
 const horario= prompt(" horario atual ? ")
 
+function saudacao(hora, nome) {
+    const mensagem = (hora >= 6 && hora < 12) ? "Bom dia" :
+                     (hora >= 12 && hora < 18) ? "Boa tarde" : 
+                     
+             
+
+   
+"Boa noite";
+    
+    
+console.log(`${mensagem}, ${nome}!`);
+}
+
+
+}
+// Exemplo de uso
+const horaAtual = new Date().getHours();
+const nomeUsuario = "João";
