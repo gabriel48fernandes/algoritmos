7a6ef4d4a07efc00b1a45289b46042ba18686b66
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2024-09-24 21:43:08 -0300

    Add files via upload

--- algoritimo/saudar.js
+++ algoritimo/saudar.js
@@ -1,27 +1,22 @@
 const prompt=require("prompt-sync")()
+function saudar(nome, hora){}
+
 const nome= prompt(" qual seu nome ? ")
-const horario= prompt(" horario atual ? ")
-function saudacaoPorHorario(hora) {
-    if (hora < 12) {
-        
-        re
-return "Bom dia!";
-    } else if (hora < 18) {
-        return "Boa tarde!";
-    } else {
-        
- 
-return "Boa noite!";
-    }
+
+
+const hora= parseInt(prompt(" horario atual ? "))
+
+ if (hora>=5 && hora < 12)
+ {
+     console.log (" bom dia ", nome)
+ }
+if (hora >12 && hora <=18)
+{    
+console.log ("boa tarde" , nome)
 }
-
-// Solicita a hora ao usuário e exibe a saudação
-const hora = parseInt(prompt("Digite a hora (0-23):"));
-
-if (!isNaN(hora) && hora >= 0 && hora <= 23) {
-    alert(saudacaoPorHorario(hora));
-} else {
-    
-   
-alert("Por favor, insira uma hora válida entre 0 e 23.");
+if (  
+    (hora >18 && hora <=23)
+     ||
+    (hora > 0 && hora <= 4) ){
+    console.log ("boa noite " , nome)
 }
