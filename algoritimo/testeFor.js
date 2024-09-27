c81f692f4e82fb0d32873e61191b4d748f878b43
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2024-09-26 22:00:30 -0300

    Add files via upload

---
+++ algoritimo/testeFor.js
@@ -0,0 +1,19 @@
+//for (let i = 1; i <= 10; i= i+2) {
+  //  console.log(i); // Exibe cada número de 1 a 10
+//}
+
+// testes: //
+
+
+//for (let i= 10; i >=1; i= i-1) {
+    //console.log (i)
+////}
+//console.log ("feliz ano novo")
+ //for (let i= 1; i<=10; i++){
+    //console.log ("7x" + i*7);
+ //}
+ for  (let i = 1; i <=10; i++) 
+    {
+        console.log("8x" ,i , "=" ,i*8)
+    
+    }
