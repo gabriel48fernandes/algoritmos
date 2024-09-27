23113295d54c74a74979e5d062bad9d984053fbb
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2024-09-27 20:19:33 -0300

    Delete inverterpalavras.js

--- inverterpalavras.js
+++
@@ -1,4 +0,0 @@
-const prompt = require ('prompt-sync')()
-const palavra = prompt("Digite uma palavra:");
-const palavraInvertida = palavra.split('').reverse().join('');
-console.log(palavraInvertida);
