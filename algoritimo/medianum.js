24e1db17265c1b38507bac73f10eaffea701585e
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2024-09-30 21:14:54 -0300

    Add files via upload

---
+++ algoritimo/medianum.js
@@ -0,0 +1,10 @@
+let soma= 0
+contador=0
+
+for (let num= 1; num<=20; num++){
+    soma +=num
+contador++
+}
+let media = soma / contador
+    console.log ("a soma dos numeros de 1 a 20 é : ", soma )
+    console.log ("a media dos numeros de 1 a 20 é ", media)
