78acdff5dec4c504243886a15353e7790e36ffe0
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2024-10-01 22:00:49 -0300

    Add files via upload

---
+++ algoritimo/testearray2.js
@@ -0,0 +1,12 @@
+contador=0
+let lista =[2, 5, 8, 11, 14, 17, 20]
+for(let i=0; i <lista.length; i++){
+    if (lista [i]%2===0){
+        contador++
+     console.log (lista[i])
+    }
+
+}      
+
+    console.log ("total de pares", contador)
+
