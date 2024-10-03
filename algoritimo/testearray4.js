b776aa73b8aa5794c93aca305573345158ac1f18
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2024-10-03 20:57:59 -0300

    Add files via upload

---
+++ algoritimo/testearray4.js
@@ -0,0 +1,6 @@
+let palavras = ["casa", "café", "livro", "senac", "programador"]
+
+for (let i = 0; i < palavras.length; i++) {
+ 
+console.log(palavras[i] + " tem " + palavras[i].length + " letras ")
+}
