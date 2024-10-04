7d07de58a8c0dda0767ed49ae2d6baa907161d01
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2024-10-03 21:11:51 -0300

    Update testearray4.js

--- algoritimo/testearray4.js
+++ algoritimo/testearray4.js
@@ -1,6 +1,10 @@
 let palavras = ["casa", "café", "livro", "senac", "programador"]
+let quantidade = 0
 
 for (let i = 0; i < palavras.length; i++) {
+quantidade += palavras[i].length
  
 console.log(palavras[i] + " tem " + palavras[i].length + " letras ")
+
 }
+console.log (quantidade)
