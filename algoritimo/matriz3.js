1ba65044cf25ee4d057b06fab43828960ef9f869
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2024-10-18 21:52:17 -0300

    Update matriz3.js

--- algoritimo/matriz3.js
+++ algoritimo/matriz3.js
@@ -1,17 +1,17 @@
-const prompt = require("prompt-sync")();
+const prompt = require("prompt-sync")()
 
 // Lista de países e populações
 let paisesPopulacao = [
-    ["Brasil", 213993437],
-    ["Estados Unidos", 331002651],
-    ["China", 1439323776],
-    ["Índia", 1380004385],
-    ["Japão", 126476461],
-    ["Alemanha", 83783942],
-    ["França", 65273511],
-    ["Reino Unido", 67886011],
-    ["Itália", 60244639],
-    ["Canadá", 37742154]
+    {nomePais: "Brasil", populaçao: 213993437},
+    {nomePais: "Estados Unidos", populaçao: 331002651},
+    {nomePais: "China", populaçao: 1439323776},
+    {nomePais: "Índia", populaçao: 1380004385},
+    {nomePais: "Japão", populaçao: 126476461},
+    {nomePais: "Alemanha", populaçao: 83783942},
+    {nomePais: "França", populaçao: 65273511},
+    {nomePais: "Reino Unido", populaçao: 67886011},
+    {nomePais: "Itália", populaçao: 60244639},
+    {nomePais: "Canadá", populaçao: 37742154}
 ];
 
 // Função para encontrar o país com maior população
@@ -20,35 +20,42 @@ function paisComMaiorPopulacao() {
     let maiorPopulacao = 0;
 
     for (let i = 0; i < paisesPopulacao.length; i++) {
-        if (paisesPopulacao[i][1] > maiorPopulacao) {
-            maiorPopulacao = paisesPopulacao[i][1];
-            paisComMaishabitantes = paisesPopulacao[i][0];
+        if (paisesPopulacao[i].populaçao > maiorPopulacao) {
+            maiorPopulacao = paisesPopulacao[i].populaçao; // Corrigido para atribuir a população
+            paisComMaishabitantes = paisesPopulacao[i].nomePais;
         }
     }
     console.log("Maior população: " + paisComMaishabitantes + " (" + maiorPopulacao + " habitantes)");
 }
 
-// Função para adicionar país (apenas população)
 function adicionarPais() {
     let nomePais = prompt("Digite o nome do país que deseja adicionar: ");
     let habitantes = parseInt(prompt("Digite a população do país: "));
+    let paisExiste = paisesPopulacao.some(pais => pais.nomePais.toLowerCase() === nomePais.toLowerCase());
 
-    // Adiciona o país na lista de populações
-    paisesPopulacao.push([nomePais, habitantes]);
-
+    if (paisExiste) {
+        console.log("O país " + nomePais + " já está cadastrado");
+    } else {
+        // Adiciona o país na lista de populações
+        paisesPopulacao.push({nomePais: nomePais, populaçao: habitantes});
+        console.log("País " + nomePais + " com " + habitantes + " habitantes foi adicionado.\n");
+    }
+    
     // Ordena a lista de países por população em ordem decrescente
-    paisesPopulacao.sort((a, b) => b[1] - a[1]);
-
-    console.log("País " + nomePais + " com " + habitantes + " habitantes foi adicionado.\n");
+    paisesPopulacao.sort((a, b) => b.populaçao - a.populaçao);
 }
-function excluirPais (){
-    let paisApagado= prompt("qual pais voce deseja excluir?")
-    for (let i=0; i<paisesPopulacao.length; i++){
-        if (paisesPopulacao[i][0]== (paisApagado))
-            paisesPopulacao.splice (i, 1)
+
+function excluirPais() {
+    let paisApagado = prompt("Qual país você deseja excluir? ");
+    let paisIndex = paisesPopulacao.findIndex(pais => pais.nomePais.toLowerCase() === paisApagado.toLowerCase());
+
+    if (paisIndex !== -1) {
+        paisesPopulacao.splice(paisIndex, 1);
+        console.log("País " + paisApagado + " excluído com sucesso.");
+    } else {
+        console.log("País não encontrado.");
     }
 }
-
 
 // Função para pesquisar por população
 function pesquisarPais() {
@@ -57,8 +64,8 @@ function pesquisarPais() {
 
     // Pesquisa na lista de populações
     for (let i = 0; i < paisesPopulacao.length; i++) {
-        if (paisesPopulacao[i][0].toLowerCase() === nomePais.toLowerCase()) {
-            console.log("País: " + paisesPopulacao[i][0] + ", População: " + paisesPopulacao[i][1]);
+        if (paisesPopulacao[i].nomePais.toLowerCase() === nomePais.toLowerCase()) {
+            console.log("País: " + paisesPopulacao[i].nomePais + ", População: " + paisesPopulacao[i].populaçao);
             encontradoPopulacao = true;
             break;
         }
@@ -75,10 +82,10 @@ function menu() {
     do {
         console.log("\n--- MENU ---")
         console.log("1. Adicionar um país")
-        console.log ("2. Excluir país " )
+        console.log("2. Excluir país")
         console.log("3. Pesquisar um país")
         console.log("4. Mostrar o país com maior população")
-        console.log("5. Mostrar todos os países e populações em ordem decrecente")
+        console.log("5. Mostrar todos os países e populações em ordem decrescente")
         console.log("6. Sair")
 
         opcao = prompt("Digite o número da opção desejada: ")
@@ -88,8 +95,8 @@ function menu() {
                 adicionarPais();
                 break;
             case "2":
-                excluirPais()
-                break
+                excluirPais();
+                break;
             case "3":
                 pesquisarPais();
                 break;
@@ -97,11 +104,9 @@ function menu() {
                 paisComMaiorPopulacao();
                 break;
             case "5":
-                // Ordena a lista de países por população em ordem decrescente
-                paisesPopulacao.sort((a, b) => b[1] - a[1]);
                 console.table(paisesPopulacao);
                 break;
-            case "5":
+            case "6":
                 console.log("Saindo do programa.");
                 break;
             default:
